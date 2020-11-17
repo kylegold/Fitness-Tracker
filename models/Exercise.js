cc73dd48bfbd4e9664ddf8f6de8767a390a18678
kyle <kyle@kyle.nyc>
2020-11-17 15:08:56 -0500

    started homework.. created models.. created base server setup

---
+++ models/Exercise.js
@@ -0,0 +1,35 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const ExerciseSchema = new Schema({
+  "type": {
+    type: String,
+    required: true
+  },
+  "name": {
+    type: String,
+    required: true
+  },
+  "duration": {
+    type: Number,
+    required: true
+  },
+  "distance": {
+    type: Number
+  },
+  "weight": {
+    type: Number
+  },
+  "reps": {
+    type: Number
+  },
+  "sets": {
+    type: Number
+  }
+
+});
+
+const Exercise = mongoose.model("Exercise", WorkoutSchema);
+
+module.exports = Exercise;
